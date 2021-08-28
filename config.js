b9d6f3eb811c9f4eed49ee38701625c3a0af90d1
phucmrc <86850668+phucmrc@users.noreply.github.com>
2021-08-28 22:53:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Fank ơiii!',
+    introTitle: 'Chị Oanh ơiii!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích cậu <33',
-    btnNo: 'Tớ chỉ coi cậu là bạn:3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại thích tớ <3',
-    btnReply: 'Gửi cho tớ <3',
+    btnYes: 'Vẫn cứ là thích em <33',
+    btnNo: 'chị chỉ coi em là bạn:3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao chị lại thích em <3',
+    btnReply: 'Gửi cho em đi <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'Làm người yêu tớ nhaaa 😘😘.',
+    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
+    messDesc: 'Làm người yêu em nhaaa 😘😘.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://https://www.facebook.com/phuc.nguyentruong.9066
 }
